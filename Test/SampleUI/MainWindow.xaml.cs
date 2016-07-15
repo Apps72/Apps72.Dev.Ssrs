2195923bd802a723445c83044718a7365326deae
Denis Voituron <denis.voituron@trasys.be>
2016-07-15 16:00:09 +0200

    NEW: Saving Url and Path into the local Registry

--- Test/SampleUI/MainWindow.xaml.cs
+++ Test/SampleUI/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Apps72.Dev.Ssrs.ReportViewer.SampleUI
 {
@@ -22,14 +11,39 @@ namespace Apps72.Dev.Ssrs.ReportViewer.SampleUI
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Loaded += (sender, e) =>
+            {
+                txtUrl.Text = Registry.GetStaticValue("Options", "Url");
+                txtPath.Text = Registry.GetStaticValue("Options", "Path");
+
+                if (String.IsNullOrEmpty(txtUrl.Text)) txtUrl.Text = "http://YourServer/ReportServer";
+                if (String.IsNullOrEmpty(txtPath.Text)) txtPath.Text = "/YourFolder/PageName";
+            };
+
+            this.Closing += (sender, e) =>
+            {
+                Registry.SetStaticValue("Options", "Url", txtUrl.Text);
+                Registry.SetStaticValue("Options", "Path", txtPath.Text);
+            };
         }
 
+        /// <summary>
+        /// Display the report
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnDisplay_Click(object sender, RoutedEventArgs e)
         {
             ReportWindow report = new ReportWindow(new Uri(txtUrl.Text), txtPath.Text);            
             report.ShowDialog();
         }
 
+        /// <summary>
+        /// Save the report
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
---
+++ Test/SampleUI/Registry.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Apps72.Dev.Ssrs.ReportViewer.SampleUI
+{
+    /// <summary>
+    /// Class to load and save configuration data in the user registry
+    /// at HKEY_CURRENT_USER\Software\Apps72\ or defined by the Root property
+    /// </summary>
+    public class Registry : IDisposable
+    {
+        #region DECLARATION 
+
+        private const string _defaultRoot = @"Software\Apps72\";
+        private const string _defaultApp = "SsrsReportViewer";
+        private const string _defaultSection = "";
+
+        private Microsoft.Win32.RegistryKey _currentSubKey;
+        private string _root = _defaultRoot;
+        private string _application = String.Empty;
+        private string _section = String.Empty;
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// Initialize a new instance of registry
+        /// </summary>
+        public Registry() : this(_defaultRoot, _defaultApp, _defaultSection)
+        {
+        }
+
+        /// <summary>
+        /// Initialize a new instance of registry
+        /// </summary>
+        /// <param name="application">Software name</param>
+        /// <param name="section">Configuration section name</param>
+        public Registry(string application, string section) : this(_defaultRoot, application, section)
+        {
+        }
+
+        /// <summary>
+        /// Initialize a new instance of registry
+        /// </summary>
+        /// <param name="root">Root name (ex. Software\Apps72)</param>
+        /// <param name="application">Software name</param>
+        /// <param name="section">Configuration section name</param>
+        public Registry(string root, string application, string section)
+        {
+            this.Root = root;
+            this.Application = application;
+            this.Section = section;
+        }
+
+        #endregion
+
+        #region PROPERTIES
+
+        /// <summary>
+        /// Gets or sets the root folder
+        /// </summary>
+        public string Root
+        {
+            set
+            {
+                if (value.EndsWith("\\"))
+                    _root = value;
+                else
+                    _root = value + "\\";
+
+                ChangeCurrentSubKey();
+
+            }
+            get
+            {
+                return _root;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the software name
+        /// </summary>
+        public string Application
+        {
+            set
+            {
+                if (value.EndsWith("\\"))
+                    _application = value;
+                else
+                    _application = value + "\\";
+
+                ChangeCurrentSubKey();
+            }
+            get
+            {
+                return _application;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the section configuration name
+        /// </summary>
+        public string Section
+        {
+            set
+            {
+                if (value.EndsWith("\\"))
+                    _section = value;
+                else
+                    _section = value + "\\";
+
+                ChangeCurrentSubKey();
+            }
+            get
+            {
+                return _section;
+            }
+        }
+
+        /// <summary>
+        /// Get the count of items included in this section
+        /// </summary>
+        public int KeysCount
+        {
+            get
+            {
+                if (_currentSubKey != null)
+                    return _currentSubKey.ValueCount;
+                else
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets all keys included in this section
+        /// </summary>
+        public string[] Keys
+        {
+            get
+            {
+                if (_currentSubKey != null)
+                    return _currentSubKey.GetValueNames();
+                else
+                    return new string[] { };
+            }
+        }
+
+        /// <summary>
+        /// Gets the count of sub sections included in this section
+        /// </summary>
+        [SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly")]
+        public int SubKeysCount
+        {
+            get
+            {
+                if (_currentSubKey != null)
+                    return _currentSubKey.SubKeyCount;
+                else
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// Gets the a list of sub sections included in this section
+        /// </summary>
+        [SuppressMessage("Microsoft.Naming", "CA1702:CompoundWordsShouldBeCasedCorrectly")]
+        public string[] SubKeys
+        {
+            get
+            {
+                if (_currentSubKey != null)
+                    return _currentSubKey.GetSubKeyNames();
+                else
+                    return new string[] { };
+            }
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// Gets the key value associated to Root, Application and Section
+        /// </summary>
+        /// <param name="key">Nom de la clé</param>
+        /// <returns>Valeur de la clé</returns>
+        public string GetValue(string key)
+        {
+            if (_currentSubKey != null)
+                return _currentSubKey.GetValue(key, String.Empty).ToString();
+            else
+                return string.Empty;
+        }
+
+        /// <summary>
+        /// Sets or create the value associated to Root, Application and Section
+        /// </summary>
+        /// <param name="key">Key name</param>
+        /// <param name="value">Value</param>
+        public void SetValue(string key, string value)
+        {
+            Microsoft.Win32.RegistryKey reg = Microsoft.Win32.Registry.CurrentUser;
+            reg = reg.CreateSubKey(_root + _application + _section);
+            reg.SetValue(key, value);
+        }
+
+        /// <summary>
+        /// Delete the key associated to Root, Application and Section
+        /// </summary>
+        /// <param name="key">Key name</param>
+        public void DeleteValue(string key)
+        {
+            Microsoft.Win32.RegistryKey reg = Microsoft.Win32.Registry.CurrentUser;
+            reg = reg.CreateSubKey(_root + _application + _section);
+            reg.DeleteSubKey(key);
+        }
+
+        /// <summary>
+        /// Delete the current section (Root, Application and Section)
+        /// </summary>
+        public void DeleteSection()
+        {
+            Microsoft.Win32.RegistryKey reg = Microsoft.Win32.Registry.CurrentUser;
+            reg.DeleteSubKeyTree(_root + _application + _section);
+        }
+
+        /// <summary>
+        /// Dispose all resources
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Dispose all resources
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Free other state (managed objects).                
+            }
+
+            // Free your own state (unmanaged objects).
+            // Set large fields to null.
+            if (_currentSubKey != null)
+            {
+                _currentSubKey.Close();
+                _currentSubKey.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// finalization code.
+        /// </summary>
+        ~Registry()
+        {
+            // Simply call Dispose(false).
+            Dispose(false);
+        }
+
+        #endregion
+
+        #region STATICS
+
+        /// <summary>
+        /// Gets the key value associated to 'HKCU\Software\Apps72\SsrsReportViewer\<see cref="Section"/>'
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string GetStaticValue(string section, string key)
+        {
+            using (Registry registry = new Registry(_defaultApp, section))
+            {
+                return registry.GetValue(key);
+            }
+        }
+
+        /// <summary>
+        /// Sets the key value associated to 'HKCU\Software\Apps72\SsrsReportViewer\<see cref="Section"/>'
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static void SetStaticValue(string section, string key, string value)
+        {
+            using (Registry registry = new Registry(_defaultApp, section))
+            {
+                registry.SetValue(key, value);
+            }
+        }
+
+
+        /// <summary>
+        /// Gets the key value as a bool (default false) associated to 'HKCU\Software\Apps72\SsrsReportViewer\<see cref="Section"/>'
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool GetStaticBooleanValue(string section, string key)
+        {
+            using (Registry registry = new Registry(_defaultApp, section))
+            {
+                string boolString = registry.GetValue(key);
+
+                bool boolValue;
+                if (Boolean.TryParse(boolString, out boolValue))
+                {
+                    return boolValue;
+                }
+
+                return false;
+            }
+        }
+
+
+        #endregion
+
+        #region PRIVATES
+
+        /// <summary>
+        /// Change the current key to Root / Application / Section
+        /// </summary>
+        private void ChangeCurrentSubKey()
+        {
+            _currentSubKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(_root + _application + _section);
+        }
+
+        #endregion
+
+    }
+}
